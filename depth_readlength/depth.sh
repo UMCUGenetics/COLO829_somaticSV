2b7daba97ef36ee03290845c352ea12431bc6fc4
jaesvi <jespejov@umcutrecht.nl>
2020-08-05 19:04:17 +0200

    bionano depth

--- depth_readlength/depth.sh
+++ depth_readlength/depth.sh
@@ -1,17 +1,26 @@
 #! /bin/sh
 
 SAMBAMBA=/hpc/local/CentOS7/cog_bioinf/sambamba_v0.6.5/sambamba
-
+BEDTOOLS=/hpc/local/CentOS7/cog_bioinf/bedtools-2.25.0/bin/bedtools
 #1,000,000 random positions
 BED=human_hg19.bed
 
 
-for TECH in nanopore pacbio
+
+
+for TECH in illumina nanopore pacbio tenx
 do
         for TYPE in tumor normal
                 BAM=${TECH}.${TYPE}.bam
-                $SAMBAMBA depth base -t 4 --min-coverage=0 -L $BED > ${BAM}.depth
+                $SAMBAMBA depth base -t 4 --min-coverage=0 -L $BED ${BAM} > ${BAM}.depth
         done
 done
+
+#Convert bionano xmaps to BED and calculate depth with bedtools coverage
+cut -f 3,6,7 bionano.tumor.xmap | grep ^[0-9] | sed s/\\.[0-9]//g | sed 's/^23/X/g' | sed 's/^24/Y/g' > bionano.tumor.bed
+$BEDTOOLS coverage -a $BED -b bionano.tumor.bed > bionano.tumor.depth
+cut -f 3,6,7 bionano.normal.xmap | grep ^[0-9] | sed s/\\.[0-9]//g | sed 's/^23/X/g' | sed 's/^24/Y/g' > bionano.normal.bed
+$BEDTOOLS coverage -a $BED -b bionano.normal.bed > bionano.normal.depth
+
 
 Rscript depth_plot.R
